c951665eecf1e33866c1eca71bbee1d7bd9a87e7
BrianFreemanJr <lordbrianjr@gmail.com>
2022-12-09 04:05:47 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  * Return: Always 0 (Success)
  */
 
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+ *
+ *  * main - Entry point
+ *
+ *   * Return: Always 0 (Success)
+ *
+ *    */
+
+
 
 int main(void)
+
 {
-<<<<<<< HEAD
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
 
-	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-        printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return(0);
-=======
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
+		int a;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);
->>>>>>> b6a2e97694086900624982e88bd439095bc519fe
+			long int b;
+
+				long long int c;
+
+					char d;
+
+						float f;
+
+
+
+							printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+
+								printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+
+									printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+
+										printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+
+											printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+												return (0);
+
 }
+
+
