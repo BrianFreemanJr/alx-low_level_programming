237ca1f5d763fd5a0c538a2201fd21a84436df5a
BrianFreemanJr <lordbrianjr@gmail.com>
2022-12-09 08:26:15 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 
 /**
  *main - Entry point
- *Return: Always 0 (Success)
-*/
+ *Return: 1 (Success)
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
