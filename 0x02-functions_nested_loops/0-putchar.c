0bf7805f4142a3ba728575b0e662327a48b1acb2
BrianFreemanJr <lordbrianjr@gmail.com>
2022-12-13 08:36:04 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description:print _putchar followed by a new line
+ * Return:0
+ */
+
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+	_putchar(c[i]);
+	i++;
+	}
+	_putchar('\n');
+	return (0);
+}
