cc8e6589e86bc0875083137d607de7235914ad2b
BrianFreemanJr <lordbrianjr@gmail.com>
2022-12-14 04:40:08 -0800

    task zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void)	
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -15,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
 }
